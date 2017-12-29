69ae2e971301e35644d2b09ea3f5348f2f5457d8
Christopher Trinh <ctrinh@bu.edu>
2017-12-28 23:16:50 -0500

    imported project files from Android Studio / created splash screen

---
+++ app/src/main/java/com/ctrinh/projectconfidant/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.ctrinh.projectconfidant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.transition);
+        tv.startAnimation(anim);
+        iv.startAnimation(anim);
+
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread() {
+            public void run () {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }; timer.start();
+    }
+}
